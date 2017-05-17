d6c7cf9fd6d892711111ae9ef02da3da205a6cd9
Rob Weber <rob.weber87@gmail.com>
2017-05-17 08:59:46 -0400

    Done.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 export default class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleEvent = this.handleEvent.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+
+  handleEvent(field, event){
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+  
+  handleFormSubmit(event) {
+    event.preventDefault()
+    const { username, password } = this.state
+    if (!username || !password) { return }
+    this.props.onSubmit({ username, password })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit.bind(this)}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.handleEvent.bind(this, 'username')}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.handleEvent.bind(this, 'password')} />
           </label>
         </div>
         <div>
@@ -29,3 +48,5 @@ export default class LoginForm extends React.Component {
     );
   }
 }
+
+LoginForm.propTypes = { onSubmit: PropTypes.func }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,14 +4,23 @@ export default class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleInput(event){
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)}/>
+        <p>Characters Remaining: {this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
